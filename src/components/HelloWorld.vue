c13c4706c7e7b344f693b55e7c5390b16854f5ac
Celine510 <celinewu1010@gmail.com>
2024-03-18 15:34:59 +0800

    feat: 🎸 test vercel

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,6 +10,7 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }} hi</h1>
+  <p>gh-pages build</p>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
